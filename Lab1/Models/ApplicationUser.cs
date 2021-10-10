8d41c21dc4fa0a2341c40cfb1d772f727ac71d7c
PatrickMalara <pat_malara@outlook.com>
2021-10-10 15:35:34 -0400

    Initial Commit Dump

---
+++ Lab1/Models/ApplicationUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab1.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required, Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required, Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required, Display(Name = "Birth Date")]
+        public DateTime BirthDate{ get; set; }
+    }
+}
---
+++ Lab1/Models/Team.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab1.Models
+{
+    public class Team
+    {
+        [Key]
+        public int Key { get; set; }
+
+        [Required, Display(Name = "Team Name")]
+        public string TeamName { get; set; }
+
+        [Required, Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Display(Name = "Established Date")]
+        public DateTime? EstablishedDate { get; set; }
+    }
+}
